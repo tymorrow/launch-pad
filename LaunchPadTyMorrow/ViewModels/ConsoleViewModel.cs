902aa0b1baec6d32148fb0232e774dec75fd26b1
Ty Morrow <tjm6f4@mst.edu>
2014-09-20 22:18:30 -0500

    Add text property/field to ConsoleViewModel and ConsoleModel.

--- LaunchPadTyMorrow/ViewModels/ConsoleViewModel.cs
+++ LaunchPadTyMorrow/ViewModels/ConsoleViewModel.cs
@@ -7,6 +7,16 @@
 	{
 		private ConsoleModel Model;
 
+		public string Text
+		{
+			get { return Model.text; }
+			set
+			{
+				Model.text = value;
+				NotifyOfPropertyChange(() => Text);
+			}
+		}
+
 		public ConsoleViewModel()
 		{
 			Model = new ConsoleModel();
