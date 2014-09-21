04000a4e977674e07144ebd9538ceaa0ad30892d
Ty Morrow <tjm6f4@mst.edu>
2014-09-20 23:28:11 -0500

    Added fields to RoverModel and corresponding API in RoverViewModel.

--- LaunchPadTyMorrow/Models/RoverModel.cs
+++ LaunchPadTyMorrow/Models/RoverModel.cs
@@ -2,5 +2,17 @@
 {
 	class RoverModel
 	{
+		internal string name;
+		internal int speed;
+		internal double temperature;
+		internal bool isConnected;
+
+		public RoverModel()
+		{
+			name = string.Empty;
+			speed = 0;
+			temperature = 0;
+			isConnected = false;
+		}
 	}
 }
--- LaunchPadTyMorrow/ViewModels/RoverViewModel.cs
+++ LaunchPadTyMorrow/ViewModels/RoverViewModel.cs
@@ -7,6 +7,43 @@
 	{
 		private RoverModel Model;
 
+		public string Name
+		{
+			get { return Model.name; }
+			set
+			{
+				Model.name = value;
+				NotifyOfPropertyChange(() => Name);
+			}
+		}
+		public int Speed
+		{
+			get { return Model.speed; }
+			set
+			{
+				Model.speed = value;
+				NotifyOfPropertyChange(() => Speed);
+			}
+		}
+		public double Temperature
+		{
+			get { return Model.temperature; }
+			set
+			{
+				Model.temperature = value;
+				NotifyOfPropertyChange(() => Temperature);
+			}
+		}
+		public bool IsConnected
+		{
+			get { return Model.isConnected; }
+			set
+			{
+				Model.isConnected = value;
+				NotifyOfPropertyChange(() => IsConnected);
+			}
+		}
+
 		public RoverViewModel()
 		{
 			Model = new RoverModel();
