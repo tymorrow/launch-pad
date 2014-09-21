0e315d66721ab87b5230a7c65897b5ed8246edb3
Ty Morrow <tjm6f4@mst.edu>
2014-09-20 22:12:44 -0500

    Cleaned up ConsoleModel.

--- LaunchPadTyMorrow/Models/ConsoleModel.cs
+++ LaunchPadTyMorrow/Models/ConsoleModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LaunchPadTyMorrow.Models
+﻿namespace LaunchPadTyMorrow.Models
 {
 	class ConsoleModel
 	{
